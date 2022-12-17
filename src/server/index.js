898bb896870bf13baa4ca37835328ec14fd670f4
alihaij <a3haij@gmail.com>
2022-12-17 17:12:00 +0300

    last commit

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,15 @@ var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
+const dotenv = require('dotenv');
+dotenv.config();
+
+console.log(`Your API key is ${process.env.API_KEY}`);
+// You could call it aylienapi, or anything else
+var textapi =  meaningCloud({
+    application_key: process.env.API_KEY
+    });
+
 const app = express()
 
 app.use(express.static('dist'))
@@ -9,13 +18,13 @@ app.use(express.static('dist'))
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    // res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
 })
 
 app.get('/test', function (req, res) {
